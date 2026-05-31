4fb1f9bb3f7b3f7efaefac63261a37495eed7d13
Fabian Barney <fabian-barney@users.noreply.github.com>
2026-05-31 16:19:22 +0200

    41 docs: add public package summaries (#80)

---
+++ contract-core/src/main/java/media/barney/contract/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Declarative contract annotations and rendering extension points for application code.
+ *
+ * <p>Use this package to annotate supported program elements and customize how masked values appear in generated
+ * violation messages.
+ */
+package media.barney.contract;
---
+++ contract-core/src/main/java/media/barney/contract/runtime/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Runtime bridge invoked by annotation-processor-generated contract checks.
+ *
+ * <p>Types in this package are part of the supported generated-code surface; applications normally use
+ * {@code media.barney.contract} annotations directly.
+ */
+package media.barney.contract.runtime;
